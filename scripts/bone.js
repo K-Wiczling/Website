6373faa90b207d3532c15a487cd5cf1dc37e1af2
K-Wiczling <wiczling.karol@gmail.com>
2022-10-15 14:50:06 +0200

    tweak

--- scripts/bone.js
+++ scripts/bone.js
@@ -1,40 +1,43 @@
-const mainContainer = document.getElementById('main-container');
+const headerElement = document.getElementById('header');
+const mainElement = document.getElementById('main');
+const footerElement = document.getElementById('footer');
 
-const main = 
+const header = 
 `
-    <header>
         <div id="logo">
             <a href="index.html">
                 <img id="logo-img" src="./assets/favicon.png">
             </a>
         </div>
         <menu id="navbar">
-            <button id="one" class="nav-bar-buttons" >All Students</button>
-            <button id="2" class="nav-bar-buttons" >Gryffindor</button>
-            <button id="3" class="nav-bar-buttons" >Slytherin</button>
-            <button id="4" class="nav-bar-buttons" >Hufflepuff</button>
-            <button id="5" class="nav-bar-buttons" >Ravenclaw</button>
-            <a href="html/blog.html"><button id="favorites" class="main-buttons" >Blog</button></a>
+            <button id="1" class="nav-bar-buttons " >Home</button>
+            <button id="2" class="nav-bar-buttons" >Projects</button>
+            <button id="3" class="nav-bar-buttons" >About Me</button>
+            <button id="4" class="nav-bar-buttons" >Contact</button>
+            <button id="4" class="nav-bar-buttons blog-btn" >Blog</button>
+
         </menu>
 
         <svg id="hamburger" fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"/></svg>
         <menu id="popup-menu">
-            <button id="one" class="nav-bar-buttons" >All Students</button>
-            <button id="2" class="nav-bar-buttons" >Gryffindor</button>
-            <button id="3" class="nav-bar-buttons" >Slytherin</button>
-            <button id="4" class="nav-bar-buttons" >Hufflepuff</button>
-            <button id="5" class="nav-bar-buttons" >Ravenclaw</button>
-            <a href="html/blog.html"><button id="favorites" class="main-buttons" >blog</button></a>
+        <button id="1" class="popup-menu-buttons " >Home</button>
+        <button id="2" class="popup-menu-buttons" >Projects</button>
+        <button id="3" class="popup-menu-buttons" >About Me</button>
+        <button id="4" class="popup-menu-buttons" >Contact</button>
+        <button id="4" class="popup-menu-buttons blog-btn" >Blog</button>
         </menu>
-    </header>
+`;
 
-    <main>
-        <h1>Karol Wiczling Personal website</h1>
-    </main>
-
-    <footer id="footer">
-        Created by Karol Wiczling 
-    </footer>
+const main = 
 `
+    <img src="assets/images/img3.jpg">
+`;
 
-mainContainer.innerHTML = main;
+const footer = 
+`
+    Created by Karol Wiczling 
+`;
+
+headerElement.innerHTML = header;
+mainElement.innerHTML = main;
+footerElement.innerHTML = footer;
