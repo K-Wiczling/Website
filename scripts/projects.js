b1e639388ee998509a5f3f16c8f53130f457c503
K-Wiczling <wiczling.karol@gmail.com>
2022-10-26 19:41:56 +0200

    working on cards

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,8 +1,67 @@
-const cards = document.getElementsByClassName('projects-card');
-for (const card of cards) {
-    card.addEventListener('click', (e) => {
-        showProject(e.target.id);
-    })
+const projectsContainer = document.getElementById('projects-contaier');
+
+const projects = [
+
+    {
+        "name": "Battleship",
+        "img": "../assets/images/img3.jpg",
+        "alt": "",
+        "projectPage": "./projects/battleship.html"
+    },
+    {
+        "name": "Snake",
+        "img": "../assets/images/img2.2.jpg",
+        "alt": "",
+        "projectPage": "./projects/battleship.html"
+    },
+    {
+        "name": "HP API",
+        "img": "../assets/images/img2.jpg",
+        "alt": "",
+        "projectPage": "./projects/battleship.html"
+    },
+    {
+        "name": "Rrobofriends",
+        "img": "../assets/images/img3.jpg",
+        "alt": "",
+        "projectPage": "./projects/battleship.html"
+    },
+    {
+        "name": "Hactoberfest",
+        "img": "../assets/images/img3.jpg",
+        "alt": "",
+        "projectPage": "./projects/battleship.html"
+    },
+]
+//Representation of the Single Card
+const card = (c) => {
+    //Single card Container
+    const cardElement = document.createElement('div');
+    cardElement.className = 'projects-card';
+
+    // Header for the card
+    const h3Element = document.createElement('h3');
+    h3Element.innerText = c.name;
+
+    //Image for the card
+    const imageElement = document.createElement('img');
+    imageElement.src = c.img;
+    
+    //Discover Button
+    const buttonElement = document.createElement('button');
+    buttonElement.innerText = 'Discover';
+    buttonElement.setAttribute( 'onclick', `showProject('${c.name}')`) ;
+
+    //Append all card children elemens 
+    cardElement.appendChild(h3Element);
+    cardElement.appendChild(imageElement);
+    cardElement.appendChild(buttonElement);
+    
+    return cardElement;
+}
+
+for (const c of projects) {
+    projectsContainer.appendChild(card(c))
 }
 
 const showProject = (e) => {
