649bd94bfefae1dd9cbe0dbdff57b82c75b386fa
K-Wiczling <wiczling.karol@gmail.com>
2022-12-03 10:24:40 +0100

    work on modal

--- scripts/pages/projects.js
+++ scripts/pages/projects.js
@@ -1,10 +1,21 @@
 import { projectsList } from '../components/projects-list.js';
 import { card } from '../components/card.js'
 
+document.getElementById('modal').style.display = 'none';
 
-const showProject = (name) => {
-    console.log(name);
+const toggleModal = (name) => {
+    if(document.getElementById('modal').style.display === 'none'){
+        document.getElementById('modal').style.display = 'block';
+    }
+    else {
+        document.getElementById('modal').style.display = 'none';
+    }
+
+    document.getElementById('modal-header').innerText = name;
 }
+
+document.getElementById('close-modal').addEventListener('click', toggleModal);
+
 for (const project of projectsList) {
-    document.getElementById('projects-contaier').appendChild(card(project, showProject));
+    document.getElementById('projects-contaier').appendChild(card(project, toggleModal));
 }
